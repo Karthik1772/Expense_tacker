e474acfbdd5080438831fa2f26f8d873c37a0f50
Karthik1772 <karthikamma2004gmail.com>
2024-06-01 21:46:50 +0530

    pending

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,20 +19,33 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
         child: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home),
-              label: "Home"
-              ),
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.graph_square_fill),
-              label: "Home"
-              ),
-          ]
+            backgroundColor: Colors.white,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.graph_square_fill), label: "Home"),
+            ]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(colors: [
+                Theme.of(context).colorScheme.tertiary,
+                Theme.of(context).colorScheme.secondary,
+                Theme.of(context).colorScheme.primary,
+              ], transform: GradientRotation(pi / 4))),
+          child: Icon(CupertinoIcons.add),
         ),
+        shape: CircleBorder(),
+        backgroundColor: Colors.red,
       ),
       backgroundColor: Colors.black,
     );
