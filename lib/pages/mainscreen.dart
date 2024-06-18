7af717d0f61c00b8cb5cd56e0c9bf8064ea7406e
Karthik1772 <karthikamma2004gmail.com>
2024-06-18 16:38:06 +0530

    pending

--- lib/pages/mainscreen.dart
+++ lib/pages/mainscreen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:expense_tracker/pages/homescreen.dart';
+import 'package:expense_tracker/pages/stats.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,39 +13,47 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-      bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.home), label: "home"),
-            BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.graph_square), label: "Stats"),
-          ]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        shape: CircleBorder(),
-        child: Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: [
-                    Theme.of(context).colorScheme.tertiary,
-                    Theme.of(context).colorScheme.secondary,
-                    Theme.of(context).colorScheme.primary,
-                  ],
-                  transform: GradientRotation(pi / 4),
-                )),
-            child: Icon(CupertinoIcons.add)),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: HomeScreen(),
-    );
+        backgroundColor: Colors.grey.shade200,
+        bottomNavigationBar: BottomNavigationBar(
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+              print(value);
+            },
+            backgroundColor: Colors.white,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            elevation: 3,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home), label: "home"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.graph_square), label: "Stats"),
+            ]),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          shape: CircleBorder(),
+          child: Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.tertiary,
+                      Theme.of(context).colorScheme.secondary,
+                      Theme.of(context).colorScheme.primary,
+                    ],
+                    transform: GradientRotation(pi / 4),
+                  )),
+              child: Icon(CupertinoIcons.add)),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        body: index == 0 ? HomeScreen() : Stats());
   }
 }
