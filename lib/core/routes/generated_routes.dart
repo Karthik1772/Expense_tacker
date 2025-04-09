8e26aacfb5ef37f5d61b882d5daf9f4bc3b8b93d
karthik1772 <karthikamma2004@gmail.com>
2025-04-09 17:41:33 +0530

    completed

--- lib/core/routes/generated_routes.dart
+++ lib/core/routes/generated_routes.dart
@@ -1,6 +1,6 @@
 
-import 'package:Xpenso/features/home_screen.dart';
-import 'package:Xpenso/features/splash.dart';
+import 'package:Xpenso/features/homeScreen/home_screen.dart';
+import 'package:Xpenso/features/splashScreen/splash.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
